3d6f24f8155b348e9a50f26cea22ec55d052929b
AlexeyYuzh19 <9676331836@mail.ru>
2022-12-26 21:19:58 +0500

    код для задачи 68 с методами

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -51,19 +51,6 @@ int CheckPositiveNumber(string text)
     return number;
 }
 
-// Код задачи
-
-metka:
-int M = CheckPositiveNumber("Введите первое неотрицательное число : ");
-
-int N = CheckPositiveNumber("Введите второе неотрицательное число : ");
-
-if (M == 0 && N == 0)
-{
-    System.Console.WriteLine("Оба числа равны 0, условие функции допускает зачение 0 для одного значения - повторите ввод чисел.");
-    goto metka;
-}
-
 int AkkermanFunction(int M, int N)
 {
     if (M == 0)
@@ -79,6 +66,23 @@ int AkkermanFunction(int M, int N)
     else
         return AkkermanFunction(M - 1, AkkermanFunction(M, N - 1));
 }
+
+// Код задачи
+
+metka:
+int M = CheckPositiveNumber("Введите первое неотрицательное число : ");
+
+int N = CheckPositiveNumber("Введите второе неотрицательное число : ");
+
+if (M == 0 && N == 0)
+{
+    System.Console.WriteLine("Оба числа равны 0, условие функции допускает зачение 0 для одного значения - повторите ввод чисел.");
+    goto metka;
+}
+
 Console.ForegroundColor = ConsoleColor.DarkCyan;
+
 System.Console.WriteLine($"\nЗначение функции Аккермана для двух неотрицательных чисел М ({M}) и N ({N}) равно : {AkkermanFunction(M, N)}\n");
+
 Console.ResetColor();
+
